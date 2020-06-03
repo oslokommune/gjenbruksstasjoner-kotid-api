28ddb7a1e5ba6ce9b75ed40345b0881e5079f161
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-06-03 12:58:16 +0200

    Initialize with flask boilerplate code

---
+++ test/test_app.py
@@ -0,0 +1,12 @@
+from app import HelloWorld
+
+
+from aws_xray_sdk.core import xray_recorder
+
+xray_recorder.begin_segment("Test")
+
+
+class TestApp:
+    def test_helloworld(self):
+        response = HelloWorld().get()
+        assert response == {"hello": "world"}
