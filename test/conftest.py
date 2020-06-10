acc4a1cfd4c3fc8b8dc094bb98e24e3cb9446f67
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-06-10 09:08:22 +0200

    DP-877: Prepare tests

---
+++ test/conftest.py
@@ -0,0 +1,40 @@
+import boto3
+import pytest
+
+# import datetime
+from moto import mock_dynamodb2
+
+from app import app as flask_app
+from queue_estimation_api.data import predictions_table_name as table_name
+
+
+def create_predictions_table(items=[], region="eu-west-1"):
+    client = boto3.client("dynamodb", region_name=region)
+    client.create_table(
+        # TODO
+    )
+
+    table = boto3.resource("dynamodb", region_name=region).Table(table_name)
+
+    for item in items:
+        table.put_item(Item=item)
+
+    return table
+
+
+@pytest.fixture
+def mock_client():
+    # Configure the application for testing and disable error catching during
+    # request handling for better reports. Required in order for exceptions
+    # to propagate to the test client.
+    # https://flask.palletsprojects.com/en/1.1.x/testing/
+    # https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.test_client
+    flask_app.config["TESTING"] = True
+
+    with flask_app.test_client() as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+def mock_dynamodb():
+    mock_dynamodb2().start()
--- test/test_app.py
+++ test/test_app.py
@@ -1,42 +1,19 @@
-import pytest
 from aws_xray_sdk.core import xray_recorder
 
-from app import app as flask_app
-from queue_estimation_api.queues_endpoint import Queues
+# from test.conftest import create_queue_times_table
 
 
 xray_recorder.begin_segment("Test")
 
 
-@pytest.fixture
-def client():
-    # db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flask_app.config["TESTING"] = True
-
-    with flask_app.test_client() as client:
-        # with flaskr.app.app_context():
-        #     flaskr.init_db()
-        yield client
-
-    # os.close(db_fd)
-    # os.unlink(flaskr.app.config['DATABASE'])
-
-
 class TestApp:
-    def test_queue(self, client):
+    def test_queues_endpoint(self, mock_client, mock_dynamodb):
+        # table = create_queue_times_table(
+        #     items=[]
+        # )
+
         # response = Queues().get(station_id="test-station-id")
-        response = client.get("/")
+        response = mock_client.get("/haraldrud")
+        # response_data = response.get_json()
 
         assert response.status_code == 200
-
-        print(response.get_json())
-
-        assert False
-
-        """
-        assert dict(response) == {
-            "station_id": "station_id_41_20200506T103000.bin",
-            "predicted_queue_time": 10.6,
-            "updated_at": "2020-06-05T09:05:12.802129",
-        }
-        """
