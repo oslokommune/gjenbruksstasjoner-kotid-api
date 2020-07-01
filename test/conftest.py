872be69dd866cf0e6b4300594cb3796eba775129
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-07-01 15:31:59 +0200

    DP-991: Inline mock data in conftest fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -5,7 +5,6 @@ import pytest
 import decimal
 from moto import mock_dynamodb2, mock_s3
 
-from test.mockdata import test_config_data, test_prediction_data
 from app import app as flask_app
 
 
@@ -76,9 +75,85 @@ def mock_s3_config():
 
 @pytest.fixture(scope="function")
 def config_data():
-    return test_config_data
+    return {
+        "prediction_config": {
+            "prediction_enabled": True,
+            "margin_of_error": 0.25,
+            "queue_full_certainty_threshold": 0.9,
+            "queue_not_full_certainty_threshold": 0.5,
+            "outdated_after_minutes": 15,
+        },
+        "stations": {
+            31: {
+                "station_name": "Haraldrud hageavfallsmottak",
+                "opening_hours": {
+                    "default": {"open": "07:00", "closed": "21:00"},
+                    "saturday": {"open": "07:00", "closed": "17:00"},
+                    "sunday": None,
+                },
+            },
+            41: {
+                "station_name": "Grønmo",
+                "opening_hours": {
+                    "default": {"open": "07:00", "closed": "21:00"},
+                    "saturday": {"open": "07:00", "closed": "17:00"},
+                    "sunday": None,
+                },
+            },
+            42: {
+                "station_name": "Haraldrud gjenbruksstasjon",
+                "opening_hours": {
+                    "default": {"open": "07:00", "closed": "21:00"},
+                    "saturday": {"open": "07:00", "closed": "17:00"},
+                    "sunday": None,
+                    "deviations": {
+                        "2020-12-24": None,
+                        "2020-06-11": {"open": "08:00", "closed": "16:00"},
+                        "2020-06-17": {"open": "08:00", "closed": "12:00"},
+                    },
+                },
+                "prediction_config": {
+                    "prediction_enabled": True,
+                    "margin_of_error": 0.3,
+                    "queue_full_certainty_threshold": 0.9,
+                    "queue_not_full_certainty_threshold": 0.5,
+                    "outdated_after_minutes": 20,
+                },
+            },
+            43: {
+                "station_name": "Smestad",
+                "opening_hours": {},
+                "prediction_config": {"prediction_enabled": False},
+            },
+            44: {
+                "station_name": "Heggvin",
+                "opening_hours": {},
+                "prediction_config": {},
+            },
+            45: {},
+        },
+    }
 
 
 @pytest.fixture(scope="function")
 def prediction_data():
-    return test_prediction_data
+    return {
+        41: {
+            "station_id": 41,
+            "queue_full": 0.1,
+            "expected_queue_time": 0.5,
+            "timestamp": 1591009200.0,  # 2020-06-01 11:00:00
+        },
+        42: {
+            "station_id": 42,
+            "queue_full": 0.74,  # UNCERTAIN
+            "expected_queue_time": 0.712,
+            "timestamp": 1591012800.0,  # 2020-06-01 12:00:00
+        },
+        31: {
+            "station_id": 31,
+            "queue_full": 0.1,
+            "expected_queue_time": 0.5,
+            "timestamp": 1591005600.0,  # 2020-06-01 10:00:00 (OUTDATED)
+        },
+    }
